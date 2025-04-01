a4886bd6b2f1d5d60ba202bbdd0e9cc1aad43dff
alicemariaarrica <alicemariaarrica@gmail.com>
2025-04-01 21:31:43 +0200

    task1

---
+++ es6.js
@@ -0,0 +1,11 @@
+
+//Crea un contatore automatico con setInterval
+//Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+
+
+function creaContatoreAutomatico(tempo) {
+
+    return setInternal(() =>
+    
+    )
+}
